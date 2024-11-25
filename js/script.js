2d10e84b30e84132e432612d43d2643c1d0f137d
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2024-11-25 17:58:17 -0500

    Create functions to perform four basic operations

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide() {
+    if (num2 === 0) {
+        return "N00000PE"
+    } else {
+        return num1 / num2;
+    }
+}

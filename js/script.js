cbcaf04372e17503ba8a6defa8508a3a1835b358
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2024-11-25 20:41:56 -0500

    HTML structure and basic CSS added
    - Create HTML elements for the user to interact with
    - Style HTML elements simply for ease of testing operations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,8 +16,27 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     if (num2 === 0) {
-        return "N00000PE"
+        return "N00000PE";
     } else {
         return num1 / num2;
     }
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
+
+num1 = 4;
+num2 = 4;
+operator = "/";
+
+console.log(operate(num1, num2, operator));
